5c09a258de54d85e2bbaa3bba3a51ff6b873877f
Jonathan Katz <jk@jknojokes.com>
2026-06-29 15:37:41 -0400

    Point /donate at custom Saratoga payment page

--- pages/donate.js
+++ pages/donate.js
@@ -10,7 +10,7 @@ export default function Donate() {
       </Head>
       <div style={{ margin: 0, padding: 0, width: '100vw', height: '100vh', overflow: 'hidden' }}>
         <iframe
-          src="https://secure.cardknox.com/saratogashteibel"
+          src="https://saratoga-payment.vercel.app"
           style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
           title="Saratoga Shteibel Donation"
         />
